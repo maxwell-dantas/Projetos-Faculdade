302f59e3da5a512ae6d1387c74f32aa13c7cade8
Maxwell Dantas <maxwelldantas.lima@gmail.com>
2023-09-24 21:45:32 -0300

    Correção

--- Prog-Comp-2023.2/Exercises/Estruturas Condicionais (if and else)/Conversão  de data - a.C to d.C.py
+++ Prog-Comp-2023.2/Exercises/Estruturas Condicionais (if and else)/Conversão  de data - a.C to d.C.py
@@ -3,12 +3,11 @@
 #Dados de entrada:
 nascimento = int(input('Informe o ano de nascimento: '))
 resultado = nascimento - 10000
-tempo_percorrido = 10000 + nascimento
 
 if nascimento < 10000:  #Condição para existir um ano a.C
      if nascimento <0:  #Condição de quem nasceu a.C*
         print(f'Nascendo no ano {nascimento*-1} a.C, caso você nascesse há 10000 anos atrás,' + 
-        f' nasceria no ano {resultado*-1} a.C\ntendo como tempo percorrido: {tempo_percorrido} anos a.C')
+        f' nasceria no ano {resultado*-1} a.C\ntendo como tempo percorrido: 10000 anos a.C')
      else:  #Condição de quem nasceu d.C
          if nascimento == 1:    #Condição para o print mostrar que passou 1 ano ao invés de 1 anos d.C
             print(f'Nascendo no ano {nascimento} d.C, caso você nascesse há 10000 anos atrás,' + 
