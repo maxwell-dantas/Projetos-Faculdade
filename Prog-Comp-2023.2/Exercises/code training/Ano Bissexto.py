40122f83c4cba2ac3ba7cef19846e1796eb040b8
Maxwell Dantas <maxwelldantas.lima@gmail.com>
2023-10-03 15:39:13 -0300

    correção

--- Prog-Comp-2023.2/Exercises/code training/Ano Bissexto.py
+++ Prog-Comp-2023.2/Exercises/code training/Ano Bissexto.py
@@ -2,10 +2,7 @@
 
 ano = int(input('Informe o ano: '))
 
-resto = ano%4
-
-if ano>=0:
-    if resto==0:
-        print('É bissexto')
-    else:
-        print('Não é bissexto')
+if ano>=0 and ano%4 == 0 and ano%100 != 0 or ano%400 == 0:
+    print('É bissexto')
+else:
+    print('Não é bissexto')
