55d6e557b8da682a974493a229906885523212b2
Maxwell Dantas <maxwelldantas.lima@gmail.com>
2023-10-12 20:32:43 -0300

    Fatorial

--- Prog-Comp-2023.2/Exercises/Laços De Repetição (while)/Fatorial.py
+++ Prog-Comp-2023.2/Exercises/Laços De Repetição (while)/Fatorial.py
@@ -1,7 +1,18 @@
 #Faça um programa que leia um número e imprima o seu fatorial.
-n = int(input('Informe um número: '))
-a = n
 
-while a>1:
-    a -= 1
-    print(f'{a}, ', end='')
+#Dado de entrada
+num = int(input('Informe um número: '))
+
+fat = num
+x = num - 1
+
+'''
+Exemplo: num = 4
+fat = 4
+x = 4 - 1 = 3
+'''
+
+while x > 0: #Condição para poder exibir o valores do fatorial
+    fat = fat * x
+    x = x - 1
+print(f'O fatorial deste número é: {fat}')
