466470817c8fe76bd1914706d35706e7032e358e
Maxwell Dantas <maxwelldantas.lima@gmail.com>
2023-10-21 20:27:58 -0300

    atualizações

---
+++ Prog-Comp-2023.2/Laços De Repetição (while)/BinarioToDecimal.py
@@ -0,0 +1,30 @@
+'''Dado um número natural na base binária, transformá-lo para a base decimal. Usar OBRIGATORIAMENTElaço de repetição. 
+Exemplo: Dado 10010 a saída será 18, pois 1.24 + 0.23 + 0.22 + 1.21 + 0.20 = 18.'''
+
+bin = str(input('Informe um valor binário: '))
+validação = True
+num = 0 #número decimal
+pot = 0 #a potência começa em 0
+rd = bin #resultado decimal
+
+for digit in bin:
+
+    if digit != '0' and digit != '1':
+
+        validação = False
+        break
+
+if validação:
+
+    bin = int(bin)
+    while bin > 0:
+
+        resto = bin % 10
+        num = num + resto * (2 ** pot)
+        bin = bin // 10
+        pot = pot + 1
+
+    print(f'O valor do número binário: {rd}, em decimal é: {num}')
+
+else:
+    print('Este valor não corresponde a um número binário!')
